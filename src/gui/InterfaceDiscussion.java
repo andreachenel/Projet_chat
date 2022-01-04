17e81ecee8c26c9746b355bfbbe3aa4016aa0952
Andréa <andrea.chenelchauvet2@gmail.com>
2022-01-04 18:25:26 +0100

    interface discussion

--- src/gui/InterfaceDiscussion.java
+++ src/gui/InterfaceDiscussion.java
@@ -1,32 +1,73 @@
 package gui;
 
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 import users.*;
+import javax.*;
 
 public class InterfaceDiscussion {
 	JFrame interfaceFrame;
-	JTextField txt;
-	JLabel conv, usrCo;
+	JTextField msg;
+	JLabel coUsr, usrCh;
 	JPanel panel;
+	JButton envoi, button, done;
+	JComboBox<String> usrComboBox;
+	//String[] usersToChoose = {"Jean", "Yves", "Steph", "Marie", "None of the listed"};
+	String[] usersToChoose;
+	
 
 	public InterfaceDiscussion() {
-		interfaceFrame = new JFrame("discussion");
-		txt = new JTextField (10);
-		conv = new JLabel("Conversations : ");
+		interfaceFrame = new JFrame("Chat");
+		msg = new JTextField (10);
+		coUsr = new JLabel("Connected Users : ");
+		envoi = new JButton ("^");
+		button = new JButton ("blabla");
+		usrCh = new JLabel ("User Choosen");
+		done = new JButton("Done");
+		
+		usersToChoose = (String[]) UserManager.getUserTab().toArray();
+		
+//		for (User u : UserManager.UserTab ) {
+//			usersToChoose.add(u.pseudo);
+//			}
+		
+		//arrayList de pseudos et après j'en fais un tableau 
+
+		usrComboBox = new JComboBox<String>(usersToChoose);
+        
+		
+       
+        done.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String selectedUser = "You selected " + usrComboBox.getItemAt(usrComboBox.getSelectedIndex());
+                panel.add(usrCh);
+                usrCh.setText(selectedUser);
+                
+            }
+        });
+    
 
 		//Creates the panel
 		panel = new JPanel(null);
 		
-		//for (int i=0; i<length(UserTab); i++);
+	
 
 		// Set bounds for every component
-		conv.setBounds(50, 50, 200, 80); //x, y, largeur, hauteur
-		txt.setBounds(60, 300, 20, 10);
+		coUsr.setBounds(30, 10, 200, 80); //x, y, largeur, hauteur
+		msg.setBounds(650, 480, 500, 20);
+		envoi.setBounds(1180, 480, 30, 20);
+		button.setBounds(30, 50, 200, 30);
+		usrCh.setBounds(90, 150, 400, 100);
+		usrComboBox.setBounds(80, 100, 140, 20);
+        done.setBounds(100, 130, 90, 20);
 
 		//Add to the panel 
-		panel.add(conv);
-		panel.add(txt);
+		panel.add(coUsr);
+		panel.add(msg);
+		panel.add(envoi);
+		panel.add(done);
+        panel.add(usrComboBox);
 		
 		interfaceFrame.getContentPane().add(panel, BorderLayout.CENTER);
 
